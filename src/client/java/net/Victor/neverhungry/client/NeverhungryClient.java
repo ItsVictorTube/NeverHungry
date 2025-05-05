8569bdde96b0900adc6c331d832d83546ab3b798
Victor <victorchou2009@outlook.com>
2025-05-04 22:13:48 -0400

    Started

---
+++ src/client/java/net/Victor/neverhungry/client/NeverhungryClient.java
@@ -0,0 +1,10 @@
+package net.Victor.neverhungry.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NeverhungryClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/net/Victor/neverhungry/Neverhungry.java
@@ -0,0 +1,15 @@
+package net.Victor.neverhungry;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Neverhungry implements ModInitializer {
+    public static final String MOD_ID = "neverhungry";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+
+    @Override
+    public void onInitialize() {
+    }
+}
